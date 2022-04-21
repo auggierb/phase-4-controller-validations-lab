1daa96efb03b65aa9ac9d85b24e9fcbb9830351d
August <auggierutter@gmail.com>
2022-04-21 18:11:57 -0400

    commit tree 0b39d047a183afdd4147f7f7618915d89896f447

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,12 +8,15 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
-
+    if author.valid? 
     render json: author, status: :created
+    else 
+      render json: {errors: author.errors}, status: :unprocessable_entity
+  end
   end
 
   private
-  
+ 
   def author_params
     params.permit(:email, :name)
   end
